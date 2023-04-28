528f3a43b24e24e1998160bf348db7e8e051bd86
Canberk Ok <canberk.ok@gmail.com>
2023-04-29 01:29:42 +0300

    Karakterler çağırıldı

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import Karakter from './components/Karakter';
 
 const App = () => {
   // Try to think through what state you'll need for this app before starting. Then build out
@@ -7,10 +8,10 @@ const App = () => {
   // Fetch characters from the API in an effect hook. Remember, anytime you have a 
   // side effect in a component, you want to think about which state and/or props it should
   // sync up with, if any.
-
   return (
     <div className="App">
-      <h1 className="Header">Karakterler</h1>
+      <h1 className="Header">React Wars</h1>
+      <Karakter />
     </div>
   );
 }
--- src/components/Karakter.js
+++ src/components/Karakter.js
@@ -1,1 +1,34 @@
 // Karakter bileşeniniz buraya gelecek
+
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+
+const CharacterList = () => {
+  const [karakter, setKarakter] = useState([]);
+
+
+
+  useEffect(() => {
+    axios.get('https://swapi.dev/api/people/')
+      .then(response => {
+        setKarakter(response.data);
+      })
+      .catch(error => {
+        console.error(error);
+      });
+  }, []);
+
+
+  return (
+    <div>
+      <h1>Star Wars Karakterleri</h1>
+      <ul>
+        {karakter.map(item => (
+          <li key={item.name}>{item.name}</li>        ))}
+      </ul>
+      
+    </div>
+  );
+}
+
+export default CharacterList;
